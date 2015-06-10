6f9efa20a48c85eac3941588f9f638a9be704920
Benjamin Strilziw <info@internerz.de>
2015-06-10 19:42:37 +0200

    corrected class

--- Board.java
+++ Board.java
@@ -15,7 +15,7 @@ public class Board {
 	}
 	
 	public void setMark(String mark, int x, int y) throws InvalidPositionException {
-		if (!mark.equals("X") || !mark.equals("O")) {
+		if (!(mark.equals("X") || mark.equals("O"))) {
 			return;
 		}
 		
@@ -28,14 +28,14 @@ public class Board {
 		boolean hasWinner = false;
 		
 		if (
-				board[0][0].equals(board[0][1]) && board[0][1].equals(board[0][2]) ||
-				board[1][0].equals(board[1][1]) && board[1][1].equals(board[1][2]) ||
-				board[2][0].equals(board[2][1]) && board[2][1].equals(board[2][2]) ||
-				board[0][0].equals(board[1][0]) && board[1][0].equals(board[2][0]) ||
-				board[0][1].equals(board[1][1]) && board[1][1].equals(board[2][1]) ||
-				board[0][2].equals(board[1][2]) && board[1][2].equals(board[2][2]) ||
-				board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) ||
-				board[0][2].equals(board[1][1]) && board[1][1].equals(board[0][2])
+				(board[0][0].equals(board[0][1]) && board[0][1].equals(board[0][2]) && !board[0][0].equals("") && !board[0][1].equals("") && !board[0][2].equals("")) ||
+				(board[1][0].equals(board[1][1]) && board[1][1].equals(board[1][2]) && !board[1][0].equals("") && !board[1][1].equals("") && !board[1][2].equals("")) ||
+				(board[2][0].equals(board[2][1]) && board[2][1].equals(board[2][2]) && !board[2][0].equals("") && !board[2][1].equals("") && !board[2][2].equals("")) ||
+				(board[0][0].equals(board[1][0]) && board[1][0].equals(board[2][0]) && !board[0][0].equals("") && !board[1][0].equals("") && !board[2][0].equals("")) ||
+				(board[0][1].equals(board[1][1]) && board[1][1].equals(board[2][1]) && !board[0][1].equals("") && !board[1][1].equals("") && !board[2][1].equals("")) ||
+				(board[0][2].equals(board[1][2]) && board[1][2].equals(board[2][2]) && !board[0][2].equals("") && !board[1][2].equals("") && !board[2][2].equals("")) ||
+				(board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[0][0].equals("") && !board[1][1].equals("") && !board[2][2].equals("")) ||
+				(board[0][2].equals(board[1][1]) && board[1][1].equals(board[0][2]) && !board[0][2].equals("") && !board[1][1].equals("") && !board[0][2].equals(""))
 			) {
 			hasWinner = true;
 		}
--- InvalidPositionException.java
+++ InvalidPositionException.java
@@ -2,4 +2,8 @@ package BoardTest;
 
 public class InvalidPositionException extends Exception {
 	
+	public InvalidPositionException() {
+		super("Marker out of position");
+	}
+	
 }
